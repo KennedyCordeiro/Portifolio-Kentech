ebf02358ccac00c136ecdc3514930eb60dc6381a
Kennedy Cordeiro <kennedysenju@gmail.com>
2023-08-04 11:05:38 -0300

    App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,30 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import ButtonInitial from "./components/ButtonInitial";
 import { AuthProvider } from "./context/auth";
-import RoutesApp from "./routes";
+import Home from "./pages/Home";
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import backgroundHeader from "./assets/Images/fundo.jpg";
 
 function App() {
   const [count, setCount] = useState(0);
+  const parallax = useRef<IParallax>(null!)
 
   return (
-    <AuthProvider>
-      <RoutesApp id="auth"></RoutesApp>
-    </AuthProvider>
+    <Parallax pages={1} style={{ top: "0", left: "0" }}>
+      <ParallaxLayer
+        offset={0}
+        speed={1}
+        factor={1}
+        style={{
+          backgroundImage: `url(${backgroundHeader})`,
+          backgroundSize: "cover",
+          height: "100vh",
+          width: "100%",
+        }}
+      >
+        <Home></Home>
+      </ParallaxLayer>
+    </Parallax> 
   );
 }
 
