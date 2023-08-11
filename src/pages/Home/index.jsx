3f9c299012cb68810982ce425775c75310f7f702
Kennedy Cordeiro <kennedysenju@gmail.com>
2023-08-11 13:33:13 -0300

    Adição de logos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import * as C from "./Home.styles";
 import NavBar from "../../components/Navbar";
-import kentech from "../../assets/Images/kentech.png";
+import kentech from "../../assets/Images/kentech.svg";
+import NavLogo from "../../assets/Images/kentechNav.svg";
 
 const Home = () => {
   return (
     <C.Container>
       <C.Nav>
+        <img
+          src={NavLogo}
+          alt="Logo kentech"
+          style={{ width: "200px", marginLeft: "60px" }}
+        ></img>
         <NavBar></NavBar>
       </C.Nav>
       <C.ContainerHeader>
